53d2dc8e8973a1d2668037abc0f2e4f31267e946
Pos1t1veGuy <UriyDoroshenko@ya.ru>
2024-06-03 00:53:51 +0300

    queue update

--- chess/consumers.py
+++ chess/consumers.py
@@ -7,23 +7,27 @@ from AUTH.models import User
 from game.models import Game
 
 
-queue_consumers = {}
+queue_consumers = []
 
 
 class QueueConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        global queue_consumers
+
         self.user = self.scope['user']
         await self.accept()
+
         if self.user.is_authenticated:
-            if not self.user.id in cache.get('queue', []):
+            if not self.user in queue_consumers:
                 await self.add_to_queue()
 
-                self.game_info = []
-                self.searching = False
-                self.ready_to_play = False
+                self.searching = True
+                self.ready = False
                 self.clicked = False
                 self.opponent = None
-                self.max_time = None
+                self.black_list = []
+
+                self.min, self.max = 1, 60
 
                 self.matchmaking_task = asyncio.create_task(self.matchmaking())
 
@@ -48,6 +52,7 @@ class QueueConsumer(AsyncWebsocketConsumer):
                 await self.matchmaking_task
             except asyncio.CancelledError:
                 pass
+        self.close()
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -60,180 +65,57 @@ class QueueConsumer(AsyncWebsocketConsumer):
                         tmin, tmax = int(data['min']), int(data['max'])
                         if 0 < tmin <= 60 and 0 < tmax <= 60 and tmin < tmax:
                             self.min, self.max = tmin, tmax
-                            await self.add_time_queue(self.min, self.max)
 
             case 'i_am_ready':
-                if await self.game_found():
-                    if 'agreed' in data.keys():
-                        if data['agreed'] == True:
-                            self.ready_to_play = True
-                            self.clicked = True
-
-                            await (await self.opponent_consumer()).send(text_data=json.dumps({
-                                'type': 'opponent_is_ready'
-                            }))
+                self.ready = True
+                self.clicked = True
+                await self.opponent.send(text_data=json.dumps({ 'type': 'opponent_is_ready' }))
 
             case 'cancel_game':
-                if await self.game_found():
-                    print('cancel2')
-                    self.clicked = True
-                    await (await self.opponent_consumer()).send(text_data=json.dumps({
-                        'type': 'game_is_canceled'
-                    }))
-                    await self.send(text_data=json.dumps({
-                        'type': 'game_is_canceled'
-                    }))
-                    await self.cancel()
-
+                self.clicked = True
+                await self.opponent.send(text_data=json.dumps({ 'type': 'game_is_canceled' }))
+                await self.send(text_data=json.dumps({ 'type': 'game_is_canceled' }))
+                await self.cancel(ban=self.opponent.user)
 
     async def add_to_queue(self):
-        queue = cache.get('queue', [])
-
-        if self.user.is_authenticated:
-            queue.append(self.user.id)
-            queue_consumers[self.user.id] = self
-
-            cache.set('queue', queue)
+        global queue_consumers
+        
+        if self.user.is_authenticated and not self in queue_consumers:
+            queue_consumers.append(self)
         else:
             raise Exception('User is not authenticated')
 
     async def remove_from_queue(self):
-        queue = cache.get('queue', [])
+        global queue_consumers
+        
         if self.user.is_authenticated:
-            if self.user.id in queue:
-                queue.remove(self.user.id)
-            if self.user.id in queue_consumers:
-                queue_consumers.pop(self.user.id)
-
-            cache.set('queue', queue)
-        else:
-            raise Exception('User is not authenticated')
-
-    async def add_time_queue(self, num1: int, num2: int):
-        queue = cache.get('queue_times', {})
-        if self.user.is_authenticated:
-            queue[self.user.id] = [min(num1, num2), max(num1, num2)]
-            cache.set('queue_times', queue)
+            if self in queue_consumers:
+                queue_consumers.remove(self)
         else:
             raise Exception('User is not authenticated')
 
     async def matchmaking(self):
-        # it means values in (user.value - interval -> user.value + interval)
-        level_interval = 2
-        winrate_interval = 5
-        games_interval = 10
-
-        self.searching = True
-
-        if not (hasattr(self, 'min') and hasattr(self, 'max')):
-            self.min, self.max = 1, 60
-
+        global queue_consumers
+        
         while 1:
-            if self.opponent:
-                print(3)
-                if self.clicked and (await self.opponent_consumer()).clicked:
-                    print(4)
-                    if self.ready_to_play and (await self.opponent_consumer()).ready_to_play:
-                        print(5)
-                        starter_queue = cache.get('starter_queue', [])
-                        if not [self.opponent.id, self.user.id, self.max_time] in starter_queue:
-                            starter_queue.append([self.user.id, self.opponent.id, self.max_time])
-                            cache.set('queue_times', starter_queue)
-                    else:
-                        print('cancel1')
-                        await self.cancel()
-
-            elif self.searching:
-                queue = cache.get('queue', [])
-                queue_times = cache.get('queue_times', {})
-
-                for user_id in queue:
-                    if user_id != self.user.id:
-                        if user_id in queue_times.keys():
-                            usermin, usermax = queue_times[user_id]
-                        else:
-                            usermin, usermax = 1, 60
-
-                        user = await sync_to_async(User.objects.get)(id=user_id)
-
-                        if await self.user.async_level() - level_interval <= await user.async_level() <= await self.user.async_level() + level_interval:
-                            if await self.user.async_winrate()-winrate_interval <= await user.async_winrate() <= await self.user.async_winrate()+winrate_interval:
-                                if await self.user.async_games_count()-games_interval <= await user.async_games_count() <= await self.user.async_games_count()+games_interval:
-                                    intersection = list( list(set( range(usermin, usermax) ) & set( range(self.min, self.max) )) )
-                                    if len(intersection) > 0 and self.searching:
-                                        self.opponent = user
-                                        if (await self.opponent_consumer()).searching:
-                                            (await self.opponent_consumer()).searching = False
-                                            self.searching = False
-
-                                            self.max_time = sum(intersection)//len(intersection)
-                                            await self.notice_players()
-
-                        self.searching = True
-
-            await self.send(text_data=json.dumps({
-                'type': 'queue_players_info',
-                'count': len(queue_consumers),
-                'winrate': sum([ await (await sync_to_async(User.objects.get)(id=user_id)).async_winrate() for user_id in queue ])/len(queue),
-                'level': sum([ await (await sync_to_async(User.objects.get)(id=user_id)).async_level() for user_id in queue ])/len(queue),
-                'score': sum([ await (await sync_to_async(User.objects.get)(id=user_id)).async_score() for user_id in queue ])/len(queue),
-                'games': sum([ await (await sync_to_async(User.objects.get)(id=user_id)).async_games_count() for user_id in queue ])/len(queue)
-            }))
+            if queue_consumers:
+                await self.send(text_data=json.dumps({
+                    'type': 'queue_players_info',
+                    'count': len(queue_consumers),
+                    'winrate': sum([ await con.user.async_winrate() for con in queue_consumers ])/len(queue_consumers),
+                    'level': sum([ await con.user.async_level() for con in queue_consumers ])/len(queue_consumers),
+                    'score': sum([ await con.user.async_score() for con in queue_consumers ])/len(queue_consumers),
+                    'games': sum([ await con.user.async_games_count() for con in queue_consumers ])/len(queue_consumers)
+                }))
             await asyncio.sleep(1)
 
-    async def opponent_consumer(self) -> 'QueueConsumer':
-        if self.opponent:
-            if self.opponent.id in queue_consumers.keys():
-                print(1)
-                opponent_consumer = queue_consumers.get(self.opponent.id)
-                print(2)
-                return opponent_consumer if isinstance(opponent_consumer, QueueConsumer) else None
-    async def game_found(self) -> bool:
-        return ( not self.searching and not (await self.opponent_consumer()).searching ) if self.opponent else False
-
-    async def cancel(self):
-        self.game_info = []
-        self.searching = False
-        self.ready_to_play = False
+    async def cancel(self, ban: 'User' = None):
+        self.searching = True
+        self.ready = False
         self.clicked = False
         self.opponent = None
-        self.max_time = None
 
-    async def notice_players(self):
-        opponent_consumer = await self.opponent_consumer()
-        if opponent_consumer:
-            if await self.game_found():
-                if opponent_consumer.opponent == None:
-                    opponent_consumer.opponent = self.user
-                    print(4)
-                    try:
-                        await opponent_consumer.send(text_data=json.dumps({
-                            'type': 'game_found',
-                            'max_time': self.max_time,
-                            'opponent': {
-                                'name': await self.user.async_username(),
-                                'winrate': await self.user.async_winrate(),
-                                'level': await self.user.async_level(),
-                                'score': await self.user.async_score(),
-                                'avatar': (await self.user.async_avatar()).url
-                            }
-                        }))
-                    except Exception as e:
-                        print("Error sending data:", e)
-
-                    print(4.5)
-
-                    await self.send(text_data=json.dumps({
-                        'type': 'game_found',
-                        'max_time': self.max_time,
-                        'opponent': {
-                            'name': await self.opponent.async_username(),
-                            'winrate': await self.opponent.async_winrate(),
-                            'level': await self.opponent.async_level(),
-                            'score': await self.opponent.async_score(),
-                            'avatar': (await self.opponent.async_avatar()).url
-                        }
-                    }))
-                    print(5)
-                else:
-                    await self.cancel()
+        if isinstance(ban, User):
+            self.black_list.append(ban)
+        elif isinstance(self.opponent, User):
+            self.black_list.append(self.opponent)
--- game/apps.py
+++ game/apps.py
@@ -8,47 +8,91 @@ import asyncio, threading, json, random
 class GameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'game'
+    queue_consumers = []
 
     async def game_starter(self):
-        from .models import Game
+        from AUTH.models import User
 
         while True:
             from chess.consumers import queue_consumers
+            self.queue_consumers = queue_consumers
 
-            starter_queue = cache.get('starter_queue', [])
+            for con1 in self.queue_consumers:
+                for con2 in self.queue_consumers:
+                    if con1.user != con2.user and self.similar_players(con1.user, con2.user):
+                        intersection = list( list(set( range(con1.min, con1.max) ) & set( range(con2.min, con2.max) )) )
 
-            for data in starter_queue:
-                try:
-                    user1, user2, max_time = data
-                    if not (isinstance(user1, User) and isinstance(user2, User) and isinstance(max_time, (float, int))):
-                        if max_time > 0:
-                            print(f'error at game_starter: Invalid data in cache "starter_queue" {data}')
-                            break
-                except ValueError:
-                    print(f'error at game_starter: Invalid data in cache "starter_queue" {data}')
-                    break
-
-                players = random.choice([[user1, user2], [user2, user1]])
-                print(players)
-                # game = await sync_to_async(Game.objects.create)(white_player=players[0], black_player=players[1], max_time=max_time)
-                # game.save()
-
-                # print('accepted')
-                # for user in players:
-                #     if user in queue_consumers.keys():
-                #         user_consumer = queue_consumers.get(user)
-                #         await user_consumer.send(text_data=json.dumps({
-                #             'type': 'game_info',
-                #             'game_id': game.id,
-                #         }))
-                #         await user_consumer.close()
+                        if len(intersection) > 0 and not con1.user in con2.black_list and not con2.user in con1.black_list:
+                            if con1.searching and con2.searching:
+                                con1.searching, con2.searching = False, False
+                                con1.opponent, con2.opponent = con2, con1
+                                await self.notice_queue_players(con1, con2)
+                            elif (con1.clicked and not con1.ready) or (con2.clicked and not con2.ready):
+                                await con1.cancel(ban=con2.user)
+                                await con2.cancel(ban=con1.user)
+                            elif con1.clicked and con1.ready and con2.clicked and con2.ready:
+                                await self.start_game(*random.choice([[con1, con2], [con2, con1]]), await self.get_mid_time(con1, con2))
 
             await asyncio.sleep(1)
 
-    def ready(self):
-        from AUTH.models import User
+    async def start_game(self, white: 'QueueConsumer', black: 'QueueConsumer', time: int):
         from .models import Game
+        #game = await sync_to_async(Game.objects.create)(white_player=white.user, black_player=black.user, max_time=time)
+        #await sync_to_async(game.save)()
 
+        print('accepted')
+        for con in [white, black]:
+            if con in self.queue_consumers:
+                await con.send(text_data=json.dumps({
+                    'type': 'game_info',
+                    'game_id': 0#game.id,
+                }))
+                await con.disconnect()
+                self.queue_consumers.remove(con)
+
+    async def similar_players(self, user1: 'User', user2: 'User') -> bool:
+        # it means values in (user.value - interval -> user.value + interval)
+        level_interval = 2
+        winrate_interval = 5
+        games_interval = 10
+
+        if await user1.async_level() - level_interval <= await user2.async_level() <= await self.user.async_level() + level_interval:
+            if await user1.async_winrate() - winrate_interval <= await user2.async_winrate() <= await user1.async_winrate()+winrate_interval:
+                if await user1.async_games_count() - games_interval <= await user2.async_games_count() <= await user1.async_games_count() + games_interval:
+                    return True
+
+        return False
+
+    async def get_mid_time(self, con1: 'QueueConsumer', con2: 'QueueConsumer') -> int:
+        intersection = list( list(set( range(con1.min, con1.max) ) & set( range(con2.min, con2.max) )) )
+        if len(intersection) > 0:
+            return sum(intersection)//len(intersection)
+
+    async def notice_queue_players(self, con1: 'QueueConsumer', con2: 'QueueConsumer'):
+        await con1.send(text_data=json.dumps({
+            'type': 'game_found',
+            'max_time': await self.get_mid_time(con1, con2),
+            'opponent': {
+                'name': await con2.user.async_username(),
+                'winrate': await con2.user.async_winrate(),
+                'level': await con2.user.async_level(),
+                'score': await con2.user.async_score(),
+                'avatar': (await con2.user.async_avatar()).url
+            }
+        }))
+        await con2.send(text_data=json.dumps({
+            'type': 'game_found',
+            'max_time': await self.get_mid_time(con1, con2),
+            'opponent': {
+                'name': await con1.user.async_username(),
+                'winrate': await con1.user.async_winrate(),
+                'level': await con1.user.async_level(),
+                'score': await con1.user.async_score(),
+                'avatar': (await con1.user.async_avatar()).url
+            }
+        }))
+
+    def ready(self):
         def start_game_starter_loop():
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
